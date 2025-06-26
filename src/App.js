c65d61388cb9afff265dffc60b32843692956bc8
Dawid Olesinski <dawid@Dawids-MacBook-Air.local>
2025-06-26 09:05:46 +0200

    fix: Set basename in Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import FAQ from "./components/FAQ";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/design2dev">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/faq" element={<FAQ />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
